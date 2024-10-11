496067ee959d755e8da5e759523638717b9b34c8
Mounecaike <jordanl4213@gmail.com>
2024-10-11 14:56:18 +0200

    Update login.js
    Ajout de la condition pour s'assurer que tout est rempli

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -2,19 +2,25 @@ logoredirect = document.querySelector(".indexredirect").addEventListener("click"
   window.location.href = "index.html"
 })
 loginForm = document.querySelector(".loginform")
+// Envoi du formulaire
 loginForm.addEventListener("submit", function (event) {
   event.preventDefault();
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
+  if (!email || !password) {
+    alert("Veuillez remplir tous les champs.");
+    return;
+  }
+  const data = { 
+    email: email,
+    password: password,
+}
   fetch("http://localhost:5678/api/users/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      email: email,
-      password: password,
-    }),
+    body: JSON.stringify(data)
   })
     .then((response) => response.json())
     .then((data) => {
